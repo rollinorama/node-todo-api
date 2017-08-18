7f5e0a0724d89f1479cff316bf72be1a33ebd210
Bruno Rollin <bruno.rollin@esauce.com.br>
2017-08-17 22:10:09 -0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, err => {
+        res.status(400).send(err);
+    })
+})
+
 app.listen(port, () => {
     console.log(process.env.NODE_ENV);
     console.log(`Server running on port ${port}`);
