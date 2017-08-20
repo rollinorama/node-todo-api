4546ed4ce43b7ba0cfeee3dc2240327721e14273
Bruno Rollin <bruno.rollin@esauce.com.br>
2017-08-20 14:22:46 -0300

    Add test for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const expect = require('expect');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server.js');
 const { Todo } = require('./../models/todo.js');
@@ -20,7 +21,7 @@ beforeEach(done => {
 
 describe('Todo API', () => {
 
-    describe('/POST', () => {
+    describe('POST /todos', () => {
         it('Should create new todo', done => {
             let text = 'Test';
             request(app)
@@ -42,7 +43,7 @@ describe('Todo API', () => {
                         done();
                     }).catch(err => done(err));
                 })
-        })
+        });
         it('Should not create todo with invalid body data', done => {
             request(app)
                 .post('/todos')
@@ -57,11 +58,11 @@ describe('Todo API', () => {
 
                         done();
                     }).catch(err => done(err));
-                })
-        })
-    })
+                });
+        });
+    });
 
-    describe('/GET', () => {
+    describe('GET /todos', () => {
         it('Should get all todos', done => {
             request(app)
                 .get('/todos')
@@ -69,6 +70,34 @@ describe('Todo API', () => {
                 .expect(res => {
                     expect(res.body.todos.length).toBe(2);
                 })
+                .end(done);
+        });
+    });
+
+    describe('GET /todos/:id', () => {
+        it('Should get one todo by ID', done => {
+            Todo.findOne({ text: 'First todo test' }).then(todo => {
+                request(app)
+                    .get(`/todos/${todo._id}`)
+                    .expect(200)
+                    .expect(res => {
+                        expect(res.body.todo.text).toBe('First todo test');
+                    })
+                    .end(done);
+            }, err => console.log(err));
+        })
+        it('Should get a 404 when get todo with valid but not existing id', done => {
+            const id = new ObjectID().toHexString();
+            request(app)
+                .get(`/todos/${id}`)
+                .expect(404)
+                .end(done);
+        })
+        it('Should get a 404 when get todo with wrong id', done => {
+            const id = '123';
+            request(app)
+                .get(`/todos/${id}`)
+                .expect(404)
                 .end(done);
         })
     })
