1d769f24b50bf2e5a33223732784ad13bdc73391
Bruno Rollin <bruno.rollin@esauce.com.br>
2017-08-20 11:24:06 -0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose.js');
 var { Todo } = require('./models/todo.js');
@@ -30,6 +31,18 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
     });
 })
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+
+    Todo.findById(id).then(todo => {
+        if (!todo)
+            return res.status(404).send();
+        res.send({ todo });
+    }, err => res.status(400).send());
+});
 
 app.listen(port, () => {
     console.log(process.env.NODE_ENV);
